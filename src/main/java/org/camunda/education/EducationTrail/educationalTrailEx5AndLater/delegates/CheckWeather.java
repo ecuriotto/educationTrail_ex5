9a7125a2aa061cfcdf456004535c5f796ab3c6bf
ecuriotto <7021326+ecuriotto@users.noreply.github.com>
2022-02-28 17:09:34 +0200

    added 5.1 - 5.2 - 5.3 - 5.4

--- src/main/java/org/camunda/education/EducationTrail/educationalTrailEx5AndLater/delegates/CheckWeather.java
+++ src/main/java/org/camunda/education/EducationTrail/educationalTrailEx5AndLater/delegates/CheckWeather.java
@@ -1,10 +1,21 @@
 package org.camunda.education.EducationTrail.educationalTrailEx5AndLater.delegates;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import javax.inject.Named;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import camundajar.impl.com.google.gson.Gson;
+import camundajar.impl.com.google.gson.JsonObject;
+import camundajar.impl.com.google.gson.JsonParser;
 
 
 @Named("weather")
@@ -15,7 +26,37 @@ public class CheckWeather implements JavaDelegate {
 
   public void execute(DelegateExecution execution) throws Exception {
 
-    execution.setVariable("weatherGood", Boolean.FALSE);
+    List<String> myListOfStrings = new ArrayList<String>();
+    myListOfStrings.add("firstElement");
+    myListOfStrings.add("secondElement");
+
+    Map<String, Integer> myMapOfIntegers = new HashMap<String, Integer>();
+    myMapOfIntegers.put("Jenny", 35);
+    myMapOfIntegers.put("Tobias", 12);
+
+    execution.setVariable("weatherGood", Boolean.TRUE);
+    execution.setVariable("thisIsDouble", Double.valueOf("3.0"));
+    execution.setVariable("thisIsInteger", Integer.valueOf(34));
+    execution.setVariable("thisIsList", myListOfStrings);
+    execution.setVariable("thisIsMap", myMapOfIntegers);
+
+    String json = "{ \"name\": \"Enrico\", \"guitar player\": true }";
+    JsonObject personJson = new JsonParser().parse(json).getAsJsonObject();
+    execution.setVariable("personJson", personJson);
+
+    String content = "Hello World \r\nJava!\r\n";
+    String path = "app.log";
+    File myFile = null;
+    try {
+
+      // Java 7
+      Files.write(Paths.get(path), content.getBytes());
+      myFile = new File("app.log");
+      execution.setVariable("myFile", myFile);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
 
     LOGGER.info("Checking weather.........");
   }
